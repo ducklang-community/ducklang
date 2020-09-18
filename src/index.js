06a46020389852bdcc5c49fc4d05a2702856c304
Ali Clark <ali@aliclark.net>
2020-09-18 11:49:05 +0100

    Extend the language, and add post-processing steps to clean parse tree

--- src/index.js
+++ src/index.js
@@ -11,9 +11,56 @@ if (parser.results.length === 0) {
     console.error('Expected more input')
 }
 else if (parser.results.length === 1) {
+    console.log(JSON.stringify(parser.results[0], null, 2))
     console.log('Good parse');
 }
 else {
+    console.log(JSON.stringify(parser.results, null, 2))
     console.error('Ambiguous parse')
-    console.log(parser.results)
 }
+
+/*
+use software,
+    category
+
+setupHelper = function of x with y, z:
+	for each foo in x, do:
+		make foo with y, z
+
+baseConcept = import software with id: 717189784315811721173404183
+Base = use baseConcept with software, category
+base = init new Base
+
+components = import software with {
+	Blaster:        { author: 'terry', version: '2.3.0' },
+	turboConcept:   { author: 'jill',  version: '0.1.0' },
+	rocketConcept:  { author: 'alex',  version: '1.0.2' },
+	RocketFix:      { author: 'rick',  version: '0.0.2' },
+	programConcept: { author: 'clive', version: '4.0.0' },
+}
+otherwise default new of category.data
+
+BetterData = use betterDataConcept with log
+extend category.data with BetterData
+
+NicerRocketFix = rename RocketFix with
+	init: 'initRocketFix'
+
+blaster = new Blaster
+
+Turbo = use turboConcept with log
+turbo = new of Turbo
+
+basicCategory = readOnly of category with
+	names: ['data', 'number', 'literal', 'list', 'text', 'rational']
+
+note:`Important pattern here: We create a partial applied module,
+	  which still has some free dependencies.
+	  Then at 'new' time, the remaining dependencies are completed,
+	  which are the "instance" variables of the object`
+Rocket = use rocketConcept with log, category: basicCategory
+category.rocket = new category with Rocket, NicerRocketFix
+rocket = new of category.rocket with blaster, turbo, offset: 720
+
+engageBoosters of rocket with angle: 93.4, thrust: 42
+*/
