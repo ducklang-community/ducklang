410692ba1fc43bc438c088ce612fa23a363afbf6
Ali Clark <ali@aliclark.net>
2021-03-06 11:00:19 +0000

    Print 'source' command instead of running it within script

--- install.sh
+++ install.sh
@@ -9,4 +9,11 @@ npm run build
 npm run deploy
 
 printf '\n%s\n\n' 'export PATH="$HOME/.ducklang/alpha/compiler:$PATH"' >>~/.bashrc
-source ~/.bashrc
+
+# Run:
+#
+#     source ~/.bashrc
+#
+# to include 'dklg' to your path
+#
+printf '\n%s!\n%s:\n\n    %s\n\n%s.\n\n' 'Ducklang is ready' 'Run' 'source ~/.bashrc' "to include 'dklg' to your path"
